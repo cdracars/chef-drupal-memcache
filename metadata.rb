1d303eb086c7e2b102eae94340a6718aaaa8dd41
Cody Dracars <cdracars@gmail.com>
2013-03-11 12:36:19 -0500

    Fix for FC007

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "drupal"
 depends          "memcached"
-depends          "php::module_memcache"
+depends          "php"
