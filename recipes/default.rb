170e6a09975061626d3ad80aa21dfab1f22aa826
Cody Dracars <cdracars@gmail.com>
2013-06-26 09:48:24 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,10 +28,23 @@ end
 
 node.default['php']['directives'] = [:extension => memcached.so]
 
-execute "append-memcache-config-to-bottom-of-settings.php" do
-  cwd "#{ node['drupal']['dir'] }/sites/default"
-  command "cp settings.php settings.php.memcache; \
-           cat #{ node['drupal']['dir'] }/memcache.conf >> settings.php.memcache; \
-           mv settings.php.memcache settings.php;"
-  not_if "grep MemCacheDrupal settings.php"
+#execute "append-memcache-config-to-bottom-of-settings.php" do
+#  cwd "#{ node['drupal']['dir'] }/sites/default"
+#  command "cp settings.php settings.php.memcache; \
+#           cat #{ node['drupal']['dir'] }/memcache.conf >> settings.php.memcache; \
+#           mv settings.php.memcache settings.php;"
+#  not_if "grep MemCacheDrupal settings.php"
+#end
+
+ruby_block "append-memcache-config-to-bottom-of-settings.php" do
+  block do
+    file = Chef::Util::FileEdit.new("#{ node['drupal']['dir'] }/memcache.conf")
+    file.insert_line_if_no_match(
+      "// Add Memcache as the page cache handler.",
+      "\n$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
+       \n$conf['cache_default_class'] = 'MemCacheDrupal';
+       \n$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';"
+    )
+    file.write_file
+  end
 end
