51c1af268810df7b18839aadcdf5c7d0dbd1f0f8
Cody Dracars <cdracars@gmail.com>
2013-06-26 13:37:04 -0500

    setup to use the conf LWRP

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,27 +28,6 @@ end
 
 node.default['php']['directives']['extension'] = ['memcached.so']
 
-#execute "append-memcache-config-to-bottom-of-settings.php" do
-#  cwd "#{ node['drupal']['dir'] }/sites/default"
-#  command "cp settings.php settings.php.memcache; \
-#           cat #{ node['drupal']['dir'] }/memcache.conf >> settings.php.memcache; \
-#           mv settings.php.memcache settings.php;"
-#  not_if "grep MemCacheDrupal settings.php"
-#end
-
-ruby_block "append-memcache-config-to-bottom-of-settings.php" do
-  block do
-    file = Chef::Util::FileEdit.new("#{ node['drupal']['dir'] }/sites/default/settings.php")
-    file.insert_line_if_no_match(
-      "// Add Memcache as the page cache handler from ruby.",
-      "\n$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
-       \n$conf['cache_default_class'] = 'MemCacheDrupal';
-       \n$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';"
-    )
-    file.write_file
-  end
-end
-
 conf_plain_file "#{ node['drupal']['dir'] }/sites/default/settings.php" do
   pattern /\/\/ Add Memcache as the page cache handler./
   new_line "\n// Add Memcache as the page cache handler.
